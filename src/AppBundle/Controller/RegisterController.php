1c4b58d3e5f6c7f89b898876215a509d954ff0fb
daliusbu <daliusbu@gmail.com>
2018-11-28 10:57:35 +0200

    Resetting templates

--- src/AppBundle/Controller/RegisterController.php
+++ src/AppBundle/Controller/RegisterController.php
@@ -10,6 +10,9 @@ namespace AppBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class RegisterController extends AbstractController
 {
@@ -17,5 +20,30 @@ class RegisterController extends AbstractController
     public function registerAction()
     {
         
+    }
+
+    /**
+     * @param $name
+     * @param \Swift_Mailer $mailer
+     * @Route("/testing", name="testing")
+     * @return Response
+     */
+    public function testingsomething( \Swift_Mailer $mailer)
+    {
+        $message = (new \Swift_Message('Hello Email'))
+            ->setFrom('send@example.com')
+            ->setTo('recipient@example.com')
+            ->setBody(
+                $this->renderView(
+                // app/Resources/views/Emails/registration.html.twig
+                    'Emails/registration.html.twig',
+                    array('name' => 'Mane')
+                ),
+                'text/html'
+            );
+        $mailer->send($message);
+        return new Response(
+            '<html><body>Lucky number: Fucks</body></html>'
+        );
     }
 }
